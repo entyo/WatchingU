d9c421af7ae56b85e9041f4039b7a2ea3e2ac0a5
e_ntyo <entyo.contact@gmail.com>
2018-01-09 15:26:01 +0900

    :pencil2: [WIP] ユーザごとにポストが表示されるようにしたがユーザの新規追加ができていない

--- src/app/item-card-list/item-card-list.component.ts
+++ src/app/item-card-list/item-card-list.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input, OnChanges } from '@angular/core';
 
 import { Item } from '../../models/item';
-import { ItemStore } from "../shared/stores/item.store";
+import { ItemStore } from '../shared/stores/item.store';
 import { Person } from '../../models/person';
 
 // TODO: 別のServiceに切り出す
-import { SlideShareService } from "../slideshare.service";
-import { MediumService } from "../medium.service";
-import { HatenaService } from "../hatena.service";
+import { SlideShareService } from '../slideshare.service';
+import { MediumService } from '../medium.service';
+import { HatenaService } from '../hatena.service';
 
 import { Observable } from 'rxjs/Observable';
 import { TimerObservable } from 'rxjs/observable/TimerObservable';
@@ -36,33 +36,24 @@ export class ItemCardListComponent implements OnChanges {
   }
 
   ngOnChanges() {
-    // TODO: User追加時にその場でやっていた、
-    // Userに対応するItemを生成してStoreに突っ込む処理をやる
-    // User に対応したitemの取得
-    // YQLのリクエスト制限を超えないように
-
-    console.log('fired');
     // TODO: この処理を別のServiceに切り出す
-    this.itemStore.list.subscribe(storedItems => {
-      TimerObservable.create(0, 5000).subscribe(() => {
-        Observable.merge(
-          this.hatena.fetchItems(this.person.name),
-          this.medium.fetchItems(this.person.name),
-          this.slideshare.fetchItems(this.person.name)
-        ).subscribe(items => {
-          items.forEach(item => {
-            const itemUrl = item.linkToContent.toString();
-            const isAlreadyExists = storedItems
-              .map(storedItem => {
-                return storedItem.linkToContent.toString();
-              })
-              .some(url => url === itemUrl);
-            if (!isAlreadyExists) {
-              item.personId = this.person.id;
-              this.itemStore.insert(item);
-              console.log('inserted: ', item);
-            }   
-          });
+    Observable.merge(
+      this.hatena.fetchItems(this.person.name),
+      this.slideshare.fetchItems(this.person.name),
+      this.medium.fetchItems(this.person.name),
+    ).subscribe(fetchedItems => {
+      fetchedItems.forEach(fetchedItem => {
+        this.itemStore.list.subscribe(storedItems => {
+          const fetchedUrl = fetchedItem.linkToContent.toString();
+          const isAlreadyExists = storedItems
+          .map(storedItem => {
+            return storedItem.linkToContent.toString();
+          })
+          .some(storedUrl => storedUrl === fetchedUrl);
+          if (!isAlreadyExists) {
+            fetchedItem.personId = this.person.id;
+            this.itemStore.insert(fetchedItem);
+          }
         });
       });
     });
