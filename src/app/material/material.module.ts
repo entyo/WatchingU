5cf550ce46cbde80631bd893c510dc167190b925
e_ntyo <entyo.contact@gmail.com>
2018-01-09 17:31:27 +0900

    :art: 全体的なスタイル調整

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,21 +3,24 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule, MatButton } from '@angular/material/button';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule, MatToolbar } from '@angular/material/toolbar';
 @NgModule({
   imports: [
     NoopAnimationsModule,
     MatButtonModule,
     MatCardModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule
   ],
   exports: [
     NoopAnimationsModule,
     MatButtonModule,
     MatCardModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
