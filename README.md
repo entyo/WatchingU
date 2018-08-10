5a45b577335a73a0653ec8c693e0338ef128db81
e_ntyo <entyo.contact@gmail.com>
2018-08-11 01:04:53 +0900

    :sparkles: Initialize purescript@0.1.2 project(w/ halogen)

---
+++ README.md
@@ -0,0 +1,5 @@
+# WatchingU
+
+RSS reader for Internet geek.
+
+This purescript project is based on https://github.com/justinwoo/halogen-parcel-hot-reload-demo. Thank you so much :heart:
---
+++ src/Main.purs
@@ -0,0 +1,13 @@
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Button (myButton)
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
+
+main :: Effect Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI myButton unit body
