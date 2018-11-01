48502df574f716a891600cb6fd7534639398fbdd
e_ntyo <entyo.contact@gmail.com>
2018-11-01 20:41:37 +0900

    Parse API response(=String) to Self-Defined record type

--- src/UserTimeLine.purs
+++ src/UserTimeLine.purs
@@ -1,18 +1,21 @@
 module UserTimeLine where
 
-import Affjax.ResponseFormat
+-- import Affjax.ResponseFormat
 
 import Affjax (get)
 import Affjax.ResponseFormat as ResponseFormat
 import Data.Either (Either(..))
+import Data.List.NonEmpty (NonEmptyList)
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
 import Effect.Aff (Aff)
 import Effect.Console (log)
+import Foreign (ForeignError)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
-import Prelude (type (~>), Unit, bind, const, discard, pure, ($), (<>))
+import Prelude (type (~>), Unit, bind, const, discard, map, pure, ($), (<>))
+import Simple.JSON (readJSON)
 
 -- | The task component query algebra.
 data UserQuery a = Remove a | Initialize a
@@ -31,14 +34,29 @@ type HatenaRSSResponse = {
   pubDate :: String,
   title :: String,
   enclosure :: {
+    -- i.e.) "0", "1"
     length :: String,
     type :: String,
     url :: String
   }
 }
 
+type  YQLResponse = {
+  query :: {
+    count :: Int,
+    created :: String,
+    lang :: String,
+    results :: {
+      item :: Array HatenaRSSResponse
+    }
+  }
+}
+
+decode :: String -> Either (NonEmptyList ForeignError) YQLResponse
+decode = readJSON
+
 type State = {
-  res :: Maybe String,
+  res :: Maybe YQLResponse,
   loading :: Boolean
 }
 
@@ -62,7 +80,7 @@ user username =
               case state.res of
                 Nothing -> []
                 Just res ->
-                  [ HH.pre_ [ HH.code_ [ HH.text res ] ] ]
+                  [ HH.ul_ (map (\item -> (HH.li_ [ HH.p_ [ HH.text item.title ] ])) res.query.results.item) ]
            , HH.button [ HE.onClick (HE.input_ Remove) ]
            [ HH.text "削除" ] ]
 
@@ -77,6 +95,11 @@ user username =
     H.liftEffect $ log $ url
     response <- H.liftAff $ get (ResponseFormat.string) url
     case response.body of
-      Left err -> H.modify_ (_ { res = Just (printResponseFormatError err), loading = false })
-      Right body -> H.modify_ (_ { res = Just body, loading = false })
+      -- TODO: エラー処理をちゃんとやる
+      -- Left err -> H.modify_ (_ { res = Just (printResponseFormatError err), loading = false })
+      Left err -> H.modify_ (_ { res = Nothing, loading = false })
+      Right body -> case (decode body) of
+        -- TODO: エラー処理をちゃんとやる
+        Left _ -> H.modify_ (_ { res = Nothing, loading = false })
+        Right b -> H.modify_ (_ { res = Just b, loading = false })
     pure next
