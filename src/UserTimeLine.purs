e60a6f0a6262db79e62b6123d021266458c959e5
e_ntyo <entyo.contact@gmail.com>
2018-11-07 19:38:50 +0900

    Promise.all的なことをする
    TODO:
      - 76-96行目の処理は明らかにヤバそうなので直す

--- src/UserTimeLine.purs
+++ src/UserTimeLine.purs
@@ -5,6 +5,7 @@ module UserTimeLine where
 import Affjax (get)
 import Affjax.ResponseFormat as ResponseFormat
 import Control.Parallel (parTraverse)
+import Data.Array ((!!))
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
@@ -29,14 +30,15 @@ type UserSlot = H.Slot UserQuery UserMessage
 _user = SProxy :: SProxy "user"
 
 type State = {
-  res :: Maybe (YQL.Response Response),
+  hatena :: Maybe (YQL.Response Hatena.Response),
+  medium :: Maybe (YQL.Response Medium.Response),
   loading :: Boolean
 }
 
 user :: String -> H.Component HH.HTML UserQuery Unit UserMessage Aff
 user username =
   H.component
-    { initialState: const { res: Nothing, loading: false }
+    { initialState: const { hatena: Nothing, medium: Nothing, loading: false }
     , render
     , eval
     , receiver: const Nothing
@@ -50,10 +52,15 @@ user username =
     HH.li_ [ HH.text username
            , HH.p_ [ HH.text (if state.loading then "Fetching..." else "") ]
            , HH.div_
-              case state.res of
+              case state.hatena of
                 Nothing -> []
                 Just res ->
-                  [ HH.ul_ (map renderItem res.query.results.item) ]
+                  [ HH.ul_ (map renderItem (map (\x -> Hatena x) res.query.results.item)) ]
+           , HH.div_
+              case state.medium of
+                Nothing -> []
+                Just res ->
+                  [ HH.ul_ (map renderItem (map (\x -> Medium x) res.query.results.item)) ]
            , HH.button [ HE.onClick (HE.input_ Remove) ]
            [ HH.text "削除" ] ]
 
@@ -65,16 +72,28 @@ user username =
   eval (Initialize next) = do
     H.modify_ (_ { loading = true })
     let urls = map (\f -> f username) [Hatena.buildUrlByUserName, Medium.buildUrlByUserName]
+    -- TODO: MonadError でエラーハンドリング https://github.com/slamdata/purescript-aff#2-monaderror
     responses <- H.liftAff $ parTraverse (get ResponseFormat.string) urls
-    -- TODO: responsesのそれぞれのresponseについて、以下の処理をやる
-    case response of
-      -- TODO: エラー処理をちゃんとやる
-      -- Left err -> H.modify_ (_ { res = Just (printResponseFormatError err), loading = false })
-      Left err -> H.modify_ (_ { res = Nothing, loading = false })
-      Right body -> case (Hatena.decode body) of
-        -- TODO: エラー処理をちゃんとやる
-        Left _ -> H.modify_ (_ { res = Nothing, loading = false })
-        Right b -> H.modify_ (_ { res = Just b, loading = false })
+    H.modify_ (_ { loading = false })
+    -- TODO: これはヤバいから直す
+    let _hatena = responses !! 0
+    case _hatena of
+      Nothing -> H.modify_ (_ { hatena = Nothing })
+      Just hatena ->
+        case hatena.body of
+          Left err -> H.modify_ (_ { hatena = Nothing })
+          Right body -> case (Hatena.decode body) of
+            Left _ -> H.modify_ (_ { hatena = Nothing })
+            Right b -> H.modify_ (_ { hatena = Just b })
+    let _medium = responses !! 1
+    case _medium of
+      Nothing -> H.modify_ (_ { medium = Nothing })
+      Just medium ->
+        case medium.body of
+          Left err -> H.modify_ (_ { medium = Nothing })
+          Right body -> case (Medium.decode body) of
+            Left _ -> H.modify_ (_ { medium = Nothing })
+            Right b -> H.modify_ (_ { medium = Just b })
     pure next
 
 data Response = Hatena Hatena.Response | Medium Medium.Response
