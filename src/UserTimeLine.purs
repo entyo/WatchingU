a8e2c198471e6aef8092eab86f6672acf3d2b36f
e_ntyo <entyo.contact@gmail.com>
2018-11-08 15:48:19 +0900

    [WIP] Use Pattern-Guards

--- src/UserTimeLine.purs
+++ src/UserTimeLine.purs
@@ -2,7 +2,7 @@ module UserTimeLine where
 
 -- import Affjax.ResponseFormat
 
-import Affjax (get)
+import Affjax as AJ
 import Affjax.ResponseFormat as ResponseFormat
 import Control.Parallel (parTraverse)
 import Data.Array ((!!))
@@ -10,12 +10,13 @@ import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
 import Effect.Aff (Aff)
+import Effect.Console (log)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Hatena as Hatena
 import Medium as Medium
-import Prelude (type (~>), Unit, bind, const, discard, map, pure, ($))
+import Prelude (type (~>), Unit, bind, const, discard, map, otherwise, pure, ($))
 import YQL as YQL
 
 -- | The task component query algebra.
@@ -73,27 +74,11 @@ user username =
     H.modify_ (_ { loading = true })
     let urls = map (\f -> f username) [Hatena.buildUrlByUserName, Medium.buildUrlByUserName]
     -- TODO: MonadError でエラーハンドリング https://github.com/slamdata/purescript-aff#2-monaderror
-    responses <- H.liftAff $ parTraverse (get ResponseFormat.string) urls
+    responses <- H.liftAff $ parTraverse (AJ.get ResponseFormat.string) urls
+    case (parseYQLResponses responses) of
+      Left error -> H.liftEffect $ log $ error
+      Right parsed -> H.modify_ (_ parsed)
     H.modify_ (_ { loading = false })
-    -- TODO: これはヤバいから直す
-    let _hatena = responses !! 0
-    case _hatena of
-      Nothing -> H.modify_ (_ { hatena = Nothing })
-      Just hatena ->
-        case hatena.body of
-          Left err -> H.modify_ (_ { hatena = Nothing })
-          Right body -> case (Hatena.decode body) of
-            Left _ -> H.modify_ (_ { hatena = Nothing })
-            Right b -> H.modify_ (_ { hatena = Just b })
-    let _medium = responses !! 1
-    case _medium of
-      Nothing -> H.modify_ (_ { medium = Nothing })
-      Just medium ->
-        case medium.body of
-          Left err -> H.modify_ (_ { medium = Nothing })
-          Right body -> case (Medium.decode body) of
-            Left _ -> H.modify_ (_ { medium = Nothing })
-            Right b -> H.modify_ (_ { medium = Just b })
     pure next
 
 data Response = Hatena Hatena.Response | Medium Medium.Response
@@ -101,3 +86,17 @@ data Response = Hatena Hatena.Response | Medium Medium.Response
 renderItem :: forall f m. Response -> H.ComponentHTML f () m
 renderItem (Hatena response) = HH.li_ [ HH.p_ [ HH.text response.title ] ]
 renderItem (Medium response) = HH.li_ [ HH.p_ [ HH.text response.title ] ]
+
+parseYQLResponses :: Array (AJ.Response (Either AJ.ResponseFormatError String)) -> Either String { hatena :: YQL.Response Hatena.Response, medium :: YQL.Response Medium.Response }
+parseYQLResponses responses 
+  | Just _hatena <- responses !! 0
+  -- Could not match type
+  -- Either t0
+  -- with type
+  --  Record
+  , Right body <- _hatena
+  , Right hatena <- Hatena.decode body
+  , Just _medium <- responses !! 1
+  , Right body <- _medium
+  , Right medium <- Medium.decode = Right { hatena, medium }
+  | otherwise = Left "Failed to parse YQL response"
